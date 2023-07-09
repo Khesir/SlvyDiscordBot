c111b886b294db2562ea02011ccce72103a01f59
Khesir <khesir2002@gmail.com>
2023-07-09 16:35:55 +0800

    Added blackjack game commad

--- Command/FunCommands.cs
+++ Command/FunCommands.cs
@@ -32,6 +32,29 @@ namespace SlvyDiscordBot.Command
             await ctx.Channel.SendMessageAsync(embedMessage);
         }
 
+        [Command("blackjack")]
+        public async Task playBlackJack(CommandContext ctx)
+        {
+            var interactvity = ctx.Client.GetInteractivity();
+            DiscordEmoji[] optionEmojis = { DiscordEmoji.FromName(ctx.Client, ":arrow_down_small", false),
+                                            DiscordEmoji.FromName(ctx.Client, ":arrow_up_small:", false),};
+            var gameMessage = new DiscordMessageBuilder()
+                .AddEmbed(new DiscordEmbedBuilder()
+                .WithColor(DiscordColor.Azure)
+                .WithTitle("Black jack")
+                .WithDescription("blackjack game embed placeholder"));
 
+            var putReactOn = await ctx.Channel.SendMessageAsync(gameMessage);
+
+            foreach (var emoji in optionEmojis)
+            {
+                await putReactOn.CreateReactionAsync(emoji);
+            }
+            //get the reaction in order to update
+            //Figure something out that is when on click, update the embeed
+            var result = await interactvity.CollectReactionsAsync(putReactOn);
+            
+            // update embed program and proceed the game
+        }
     }
 }
