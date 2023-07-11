cb3579157fe054a54ece4c637fb711c859070d9c
Khesir <khesir2002@gmail.com>
2023-07-11 14:24:42 +0800

    Modify and added new classes for the blackjack command

--- Command/FunCommands.cs
+++ Command/FunCommands.cs
@@ -1,6 +1,7 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using SlvyDiscordBot.External_Classes;
 using DSharpPlus.Interactivity.Extensions;
 using Newtonsoft.Json.Linq;
 using System;
@@ -8,6 +9,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DSharpPlus.EventArgs;
+using DSharpPlus;
 
 namespace SlvyDiscordBot.Command
 {
@@ -35,27 +38,92 @@ namespace SlvyDiscordBot.Command
         [Command("blackjack")]
         public async Task playBlackJack(CommandContext ctx)
         {
-            var interactvity = ctx.Client.GetInteractivity();
-            DiscordEmoji[] optionEmojis = { DiscordEmoji.FromName(ctx.Client, ":arrow_down_small", false),
-                                            DiscordEmoji.FromName(ctx.Client, ":arrow_up_small:", false),};
+            var deck = new Deck();
+            deck.Shuffle();
+
+            var playerHand = new Hand();
+            var dealerHand = new Hand();
             
-            var gameMessage = new DiscordMessageBuilder()
-                .AddEmbed(new DiscordEmbedBuilder()
-                .WithColor(DiscordColor.Azure)
-                .WithTitle("Black jack")
-                .WithDescription("blackjack game embed placeholder"));
+            // Deal initial cards
+            playerHand.AddCard(deck.DrawCard());
+            playerHand.AddCard(deck.DrawCard());
 
-            var putReactOn = await ctx.Channel.SendMessageAsync(gameMessage);
+            dealerHand.AddCard(deck.DrawCard());
+            dealerHand.AddCard(deck.DrawCard());
 
-            foreach (var emoji in optionEmojis)
+            // Display initial hands
+            var playerHandMessage = await ctx.RespondAsync($"Your Hand: {playerHand} | Total = {playerHand.GetHandValue()}");
+            var dealerHandMessage = await ctx.RespondAsync($"Dealer Hand: {dealerHand.FirstCard() }| Total = {playerHand.GetHandValue()}");
+
+            //Add Reactions for player moves
+            var hitEmoji = DiscordEmoji.FromName(ctx.Client, ":punch:",false);
+            var standEmoji = DiscordEmoji.FromName(ctx.Client, ":raised_hand:", false);
+
+            await playerHandMessage.CreateReactionAsync(hitEmoji);
+            await playerHandMessage.CreateReactionAsync(standEmoji);
+            //Event handler for reaction added
+            async Task OnReactionAdded(DiscordClient sender,MessageReactionAddEventArgs e)
             {
-                await putReactOn.CreateReactionAsync(emoji);
+                if (e.User.Id != ctx.User.Id || e.Message.Id != playerHandMessage.Id)
+                    return;
+                if (e.Emoji == hitEmoji)
+                {
+                    //Player draws another card
+                    var card = deck.DrawCard();
+                    playerHand.AddCard(card);
+                    await ctx.RespondAsync($"Player draws: {card}");
+
+                    //Update player hand message
+                    await playerHandMessage.ModifyAsync($"Player Hand: {playerHand} | Total {playerHand.GetHandValue()}");
+                    
+                    // Check if player busts
+                    if(playerHand.IsBust())
+                    {
+                        await ctx.RespondAsync("Player busts! Dealer wins!");
+                        ctx.Client.MessageReactionAdded -= OnReactionAdded;
+                        return;
+                    }
+                
+                }
+                else if(e.Emoji== standEmoji)
+                {
+                    //Player stands, and the dealer plays
+                    await ctx.RespondAsync($"Player stands. Dealer reveals {dealerHand}");
+
+                    await dealerHandMessage.ModifyAsync($"Dealer Hand: {dealerHand}");
+
+                    while(dealerHand.GetHandValue() < 17) 
+                    {
+                        var card = deck.DrawCard();
+                        dealerHand.AddCard(card);
+                        await ctx.RespondAsync($"Dealer draws: {card}");
+
+                        //Update dealer hand message after each card drawn.
+
+                        await dealerHandMessage.ModifyAsync($"Dealer Hand: {dealerHand}");
+                    }
+                    // Determine the winner
+                    if(dealerHand.IsBust() || playerHand.GetHandValue() > dealerHand.GetHandValue())
+                    {
+                        await ctx.RespondAsync("Player wins!");
+                    }
+                    else if (playerHand.GetHandValue() < dealerHand.GetHandValue())
+                    {
+                        await ctx.RespondAsync("Dealer wins!");
+                    }
+                    else
+                    {
+                        await ctx.RespondAsync("It's a tie!");
+                    }
+
+                    ctx.Client.MessageReactionAdded -= OnReactionAdded;
+                    return;
+                }
+                // Remove the user's reaction
+                await playerHandMessage.DeleteReactionAsync(e.Emoji, e.User);
             }
-            //get the reaction in order to update
-            //Figure something out that is when on click, update the embeed
-            var result = await interactvity.CollectReactionsAsync(putReactOn);
-            
-            // update embed program and proceed the game
+            // Subscribe to thhe reaction added event
+            ctx.Client.MessageReactionAdded += OnReactionAdded; 
         }
     }
 }
---
+++ External Classes/Hand.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlvyDiscordBot.External_Classes
+{
+    public class Hand
+    {
+        private List<Card> cards;
+
+        public Hand()
+        {
+            cards = new List<Card>();
+        }
+        public void AddCard(Card card)
+        {
+            cards.Add(card); 
+        }
+        public int GetHandValue()
+        {
+            int value = 0;
+            int numAces = 0;
+            
+            foreach(var card in cards)
+            {
+                if (card.Rank == "Ace")
+                {
+                    value += 11;
+                    numAces++;
+                }
+                else if(card.Rank == "King" || card.Rank == "Queen" || card.Rank == "Jack")
+                {
+                    value += 10;
+                }
+                else
+                {
+                    // All number cards
+                    value += int.Parse(card.Rank);
+                }
+            }
+            while (value > 21 && numAces > 0)
+            {
+                value -= 10;
+                numAces--;
+            }
+            return value;
+        }
+        public bool HasBlackjack()
+        {
+            return cards.Count == 2 && GetHandValue() == 21;
+        }
+
+        public bool IsBust()
+        {
+            return GetHandValue() > 21;
+        }
+        public Card FirstCard()
+        {
+            return cards.First();
+        }
+        public override string ToString()
+        {
+            return string.Join(", ", cards);
+        }
+    }
+}
